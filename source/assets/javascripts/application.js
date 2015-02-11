1fd0e290a3c9c93190df497b90545a026b11027a
Adriano Fernandes <djadrianof@gmail.com>
2015-02-11 11:26:06 -0200

    created directives for offcanvas and overlay

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,21 +1,41 @@
 'use strict';
 
 angular.module('ge', [])
-  .directive('offcanvasButton', function() {
-    return {
-      restrict: 'A',
-      link: function( scope, element, attrs ) {
+  .controller('offcanvasButtonController', ['$scope', '$rootScope', function( $scope, $rootScope ) {
 
-        var body = $( 'body' );
+    $scope.toggleOffCanvasOpen = function() {
+      $rootScope.$apply(function() {
+        $rootScope.offcanvasOpened ? $rootScope.offcanvasOpened = false : $rootScope.offcanvasOpened = true;
+      })
+    };
+
+  }])
+  .directive('overlay', function() {
+    return {
+      restrict  : 'A',
+      require   : ['^?offcanvasButton'],
+      link      : function( scope, element, attrs ) {
 
         element.bind('click', function( evt ) {
-          body.toggleClass('nav-default-opened');
-        });
-
-        body.delegate('.ge-overlay', 'click', function(evt) {
-          body.toggleClass('nav-default-opened');
+          scope.toggleOffCanvasOpen();
         });
 
       }
     };
-  });
+  })
+  .directive('offcanvasButton', function() {
+    return {
+      restrict  : 'A',
+      controller: 'offcanvasButtonController',
+      link      : function( scope, element, attrs ) {
+
+        element.bind('click', function( evt ) {
+          scope.toggleOffCanvasOpen();
+        });
+
+      }
+    };
+  })
+  .run(['$rootScope', function( $rootScope ) {
+    $rootScope.offcanvasOpened = false;
+  }]);
